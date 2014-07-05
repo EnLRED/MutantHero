b408388be08122e906ff953814961f5e43794356
EyeBallHK47 <a.zamytin@yandex.ru>
2014-07-06 02:15:40 +0400

    InitFix

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,27 +4,20 @@ include("shared.lua")
 local old_time_standart = 6
 
 local ROUND_SETTIME = old_time_standart
-local ROUND_STARTED = false
 local ROUND_TIME_SECS = 0
-local ROUND_ENT = false
+
+local IS_ROUND_STARTED = false
+local IS_ROUND_END = false
 
 resource.AddWorkshop("107347296") //sunnyisles
-
-util.PrecacheSound("npc/stalker/stalker_scream1.wav")
-util.PrecacheSound("npc/crow/idle3.wav")
-util.PrecacheSound("music/hl2_song3.mp3")
-util.PrecacheSound("music/radio1.mp3")
-util.PrecacheSound("music/ravenholm_1.mp3")
-
-SetGlobalString("timetoend", tostring(ROUND_SETTIME))
-SetGlobalString("timetoendsec", tostring(SECONDS_ROUND_SETTIME))
-SetGlobalBool("round_started", ROUND_STARTED)
 
 local Models = {
 	"models/player/zombie_fast.mdl",
 	"models/player/charple.mdl"
 }
+
 util.AddNetworkString("pointshop_toserv")
+
 function GM:Initialize()
 	util.PrecacheSound("npc/stalker/stalker_scream1.wav")
 	util.PrecacheSound("npc/crow/idle3.wav")
@@ -34,11 +27,13 @@ function GM:Initialize()
 
 	SetGlobalString("timetoend", tostring(ROUND_SETTIME))
 	SetGlobalString("timetoendsec", tostring(SECONDS_ROUND_SETTIME))
-	SetGlobalBool("round_started", ROUND_STARTED)
+	
+	SetGlobalBool("round_started", IS_ROUND_STARTED)
+	SetGlobalBool("round_end", IS_ROUND_END)
 end
 
 function GM:Think()
-	net.Receive("pointshop_toserv",pshop_handler) 
+	net.Receive("pointshop_toserv", pshop_handler) 
 end
 
 function GM:PlayerSpawn(ply) --COMMMMMMMIT
@@ -48,15 +43,15 @@ function GM:PlayerSpawn(ply) --COMMMMMMMIT
 end
 
 function pshop_handler(ln, ply)
-	Class = net.ReadTable()
+	local Class = net.ReadTable()
 	
 	if Class.Cost <= ply:GetMoney() then
 		if Class.IsAmmo then
-			ply:GiveAmmo(Class.Num, Class.ClassName, True) 
-			ply:SetMoney(ply:GetMoney()-Class.Cost)
+			ply:GiveAmmo(Class.Num, Class.ClassName, true) 
+			ply:SetMoney(ply:GetMoney() - Class.Cost)
 		else
 			ply:Give(Class.ClassName)
-			ply:SetMoney(ply:GetMoney()-Class.Cost)
+			ply:SetMoney(ply:GetMoney() - Class.Cost)
 		end
 	end
 end
