73dcd77e64449b1ee5b817f28d24463fe8b4e11d
EyeBallHK47 <a.zamytin@yandex.ru>
2014-07-06 20:54:25 +0400

    aaaaa
    aaaa

--- gamemode/changeclass.lua
+++ gamemode/changeclass.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 function open_ChangeCls()
 	local win = vgui.Create("DFrame")
-	win:SetSize(420, 450)
+	win:SetSize(1100, 260)
 	win:ShowCloseButton(false)
 	win:SetTitle(" ")
 	win:Center()
@@ -16,13 +16,109 @@ function open_ChangeCls()
 	win:MakePopup()
 	
 	local close = vgui.Create("DButton", win)
-	close:SetPos(50, 25)
+	close:SetPos(390, 25)
 	close:SetSize(320, 20)
-	close:SetText("Close")
+	close:SetText("Close (If you pressed your class will be heavy)")
 	close.DoClick = function() if win and IsValid(win) then win:Close() end end
 	
+	//medic
+	local info = vgui.Create("SpawnIcon", win)
+	info:SetPos(50, 50)
+	info:SetModel("models/player/alyx.mdl")
+	info:SetSize(100, 100)
+	info.OnMousePressed = function()
+		
+		
+		return false
+	end
 	
+	local info = vgui.Create("DLabel", win)
+	info:SetPos(50, 150)
+	info:SetFont("mutanthero_verysmallf")
+	info:SetText("Classname: medic\nMedic can heal players\nMedic isn't soldier\nBut he can attack too")
+	info:SizeToContents()
+	///////
+	
+	
+	
+	//engineer
+	local info = vgui.Create("SpawnIcon", win)
+	info:SetPos(250, 50)
+	info:SetModel("models/player/eli.mdl")
+	info:SetSize(100, 100)
+	info.OnMousePressed = function()
+		
+		
+		return false
+	end
+	
+	local info = vgui.Create("DLabel", win)
+	info:SetPos(250, 150)
+	info:SetFont("mutanthero_verysmallf")
+	info:SetText("Classname: engineer\nEngineer have turrets\nEngineer isn't soldier\nBut he can attack too")
+	info:SizeToContents()
+	////////
+	
+	
+	
+	//light soldier
+	local info = vgui.Create("SpawnIcon", win)
+	info:SetPos(450, 50)
+	info:SetModel("models/Humans/Group03/male_02.mdl")
+	info:SetSize(100, 100)
+	info.OnMousePressed = function()
+		
+		
+		return false
+	end
+	
+	local info = vgui.Create("DLabel", win)
+	info:SetPos(450, 150)
+	info:SetFont("mutanthero_verysmallf")
+	info:SetText("Classname: light soldier\nFaster soldier\nCan have light guns\nCan't use big guns")
+	info:SizeToContents()
+	/////////
+	
+	
+	
+	//heavy soldier
+	local info = vgui.Create("SpawnIcon", win)
+	info:SetPos(650, 50)
+	info:SetModel("models/Humans/Group03/male_09.mdl")
+	info:SetSize(100, 100)
+	info.OnMousePressed = function()
+		
+		
+		return false
+	end
+	
+	local info = vgui.Create("DLabel", win)
+	info:SetPos(650, 150)
+	info:SetFont("mutanthero_verysmallf")
+	info:SetText("Classname: heavy soldier\nHeavy soldier is slowly\nCan have big guns and light guns\nHave big damage")
+	info:SizeToContents()
+	/////////
+	
+	
+	
+	//berserk
+	local info = vgui.Create("SpawnIcon", win)
+	info:SetPos(850, 50)
+	info:SetModel("models/odessa.mdl")
+	info:SetSize(100, 100)
+	info.OnMousePressed = function()
+		
+		
+		return false
+	end
+	
+	local info = vgui.Create("DLabel", win)
+	info:SetPos(850, 150)
+	info:SetFont("mutanthero_verysmallf")
+	info:SetText("Classname: berserk\nBerserk is faster\nHave big damage with melee weapons\n")
+	info:SizeToContents()
+	/////////
 end
 
-concommand.Add("open_shop", open_ChangeCls)
-usermessage.Hook("open_shop_muth", open_ChangeCls)
+concommand.Add("open_chngcls", open_ChangeCls)
+usermessage.Hook("open_chngcls_muth", open_ChangeCls)
